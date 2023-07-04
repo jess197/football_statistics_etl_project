c2147053e3503056a2a96c40d28080e7a2021ac3
Jess <jessicacostaesilva97@gmail.com>
2023-07-04 18:16:45 -0300

    update from blob storage to aws s3

--- data_uploader.py
+++ data_uploader.py
@@ -1,15 +1,40 @@
-from azure.storage.blob import BlobServiceClient
-import os 
+#from azure.storage.blob import BlobServiceClient
+import boto3
+import os
+
 
 class DataUploader:
     
     def __init__(self):
-        connection_string = os.getenv('CONN_STRING')
-        self.container_name = 'footballapi-data'
-        self.blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+       # connection_string = os.getenv('CONN_STRING')
+       # self.container_name = 'footballapi-data'
+       # self.blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+        self.bucket_name = 'football-data-bucket-s3'
+        self.aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID') 
+        self.aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
 
 
-    def upload_file_to_BlobStorage(self, file_name,file):
+    def upload_file_to_S3(self, file_name, file):
+        s3 = boto3.client('s3', aws_access_key_id=self.aws_access_key_id,
+                        aws_secret_access_key=self.aws_secret_access_key)
+        try:
+            s3.put_object(
+                Bucket=self.bucket_name,
+                Key=file_name,
+                Body=file
+            )
+            print(f"Uploaded {file_name} to Amazon S3 with success")
+        except Exception as e:
+            print("An error occurred during the upload of the file to Amazon S3:")
+            print(str(e))
+
+    def blob_exists(self, file_name):
+        s3_client = boto3.client('s3')
+        response = s3_client.list_objects_v2(Bucket=self.bucket_name, Prefix=file_name)
+        return 'Contents' in response
+
+    '''    
+      def upload_file_to_BlobStorage(self, file_name,file):
         blob_client = self.blob_service_client.get_blob_client(container=self.container_name,blob=file_name)
         try:
             blob_client.upload_blob(file,overwrite=True)
@@ -22,3 +47,8 @@ class DataUploader:
     def blob_exists(self,file_name):   
         blob_client = self.blob_service_client.get_blob_client(container=self.container_name,blob=file_name)
         return blob_client.exists()
+    '''
+
+    
+
+
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ uploader = DataUploader()
 def process_teams():
     response = requester.request_teams('Brazil')
     json_data = json.dumps(response['response'])
-    uploader.upload_file_to_BlobStorage('teams_brazil/teams.json',json_data)
+    uploader.upload_file_to_S3('teams_brazil/teams.json',json_data)
 
 def process_teams_statistics():
     response_league = requester.request_league(71)
@@ -29,7 +29,7 @@ def process_teams_statistics():
                 time.sleep(5)
                 response_team = requester.request_teams_statistics(team_id,71,season)
                 json_data = json.dumps(response_team['response'])
-                uploader.upload_file_to_BlobStorage(f'teams_statistics/{championship}/{season}/{team_name}/statistics.json',json_data)
+                uploader.upload_file_to_S3(f'teams_statistics/{championship}/{season}/{team_name}/statistics.json',json_data)
         else:
             continue
 
@@ -40,7 +40,7 @@ def process_standings():
         if int(year) >= 2018:
             response_standings = requester.request_standings(71,year)
             json_data = json.dumps(response_standings['response'])
-            uploader.upload_file_to_BlobStorage(f'teams_standings/{year}/standings.json',json_data)
+            uploader.upload_file_to_S3(f'teams_standings/{year}/standings.json',json_data)
         else:
             continue
 
@@ -58,10 +58,9 @@ def process_fixtures():
                 team_id = team['team']['id']
                 team_name = team['team']['name']
                 team_name = team_name.lower().replace(' ', '-')
-                time.sleep(5)
                 response_fixtures = requester.request_fixtures(71,year,team_id)
                 json_data = json.dumps(response_fixtures['response'])
-                uploader.upload_file_to_BlobStorage(f'teams_fixtures/{championship}/{season}/{team_name}/fixtures.json',json_data)
+                uploader.upload_file_to_S3(f'teams_fixtures/{championship}/{season}/{team_name}/fixtures.json',json_data)
         else:
             continue
 
@@ -91,7 +90,7 @@ def process_fixtures_statistics():
                     if not uploader.blob_exists(file_name):
                         response_fixt_statistics = requester.request_fixtures_statistics(fixture_id)
                         json_data = json.dumps(response_fixt_statistics['response'])
-                        uploader.upload_file_to_BlobStorage(file_name,json_data)
+                        uploader.upload_file_to_S3(file_name,json_data)
                     else:
                         print(f'File already found: {file_name}')
                         continue
@@ -125,7 +124,7 @@ def process_fixtures_predictions():
                         predict = response_fixt_predictions['response'][0]['predictions']
                         predict['fixture_id'] = fixture_id
                         json_data = json.dumps(predict)
-                        uploader.upload_file_to_BlobStorage(file_name,json_data)
+                        uploader.upload_file_to_S3(file_name,json_data)
                     else:
                         print(f'File already found: {file_name}')
                         continue
@@ -163,17 +162,17 @@ def process_fixtures_odds():
                     #bookmaker - bet365
                     response_fixt_odds_pre = requester.request_fixtures_odds_pre_match(fixture_id,1,8)
                     json_data = json.dumps(response_fixt_odds_pre['response'])
-                    uploader.upload_file_to_BlobStorage(file_name,json_data)
+                    uploader.upload_file_to_S3(file_name,json_data)
                 else:
                     print(f'File already found: {file_name}')
                     continue
 
 # TODO CREATE DAGS IN AIRFLOW FOR EACH BATCH OR STREAMING PROCESS  
-#process_teams()
-#process_statistics()
-#process_standings()
-#process_fixtures()
-#process_fixtures_statistics()
-#process_fixtures_predictions()
-#process_fixtures_odds()
+process_teams()
+process_teams_statistics()
+process_standings()
+process_fixtures()
+process_fixtures_statistics()
+process_fixtures_predictions()
+process_fixtures_odds()
 
