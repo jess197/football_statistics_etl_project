7679da1a5d00204d9c590b53c0fddd9b12034736
Jess <jessicacostaesilva97@gmail.com>
2023-07-19 17:04:35 -0300

    Add dbt dags to run dbt models

---
+++ dags/dag_dbt_teams_brazil.py
@@ -0,0 +1,38 @@
+from pendulum import datetime
+
+from airflow.decorators import dag
+from airflow.operators.empty import EmptyOperator
+from airflow.providers.dbt.cloud.operators.dbt import DbtCloudRunJobOperator
+
+
+
+DBT_CLOUD_CONN_ID = "dbt_airflow_conn"
+JOB_ID = "383473"
+
+
+
+@dag(
+    dag_id="dag_dbt_teams_brazil",
+    start_date=datetime(2023, 7, 18),
+    schedule_interval="@daily",
+    catchup=False,
+    default_view="graph",
+    doc_md=__doc__,
+)
+def run_dbt_teams_brazil():
+    begin, end = [EmptyOperator(task_id=id) for id in ["begin", "end"]]
+
+    teams_brazil_task = DbtCloudRunJobOperator(
+        task_id="teams_brazil_task",
+        dbt_cloud_conn_id=DBT_CLOUD_CONN_ID,
+        job_id=JOB_ID,
+        check_interval=600,
+        timeout=3600,
+    )
+
+    begin >> teams_brazil_task >> end
+
+
+dag = run_dbt_teams_brazil()
+
+
